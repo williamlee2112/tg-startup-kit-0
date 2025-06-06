8a4e09e385e2be69f6b51fc87671d4f673530654
Volo Builds <148304490+VoloBuilds@users.noreply.github.com>
2025-06-06 15:52:00 -0400

    Fix volo-app branch name

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const program = new Command();
 
 // Default template URL
 const DEFAULT_TEMPLATE = 'https://github.com/VoloBuilds/volo-app.git';
-const VOLO_APP_BRANCH = 'release/v0.1.0';
+const VOLO_APP_BRANCH = 'release/v0.2.0';
 
 export async function main() {
   program
