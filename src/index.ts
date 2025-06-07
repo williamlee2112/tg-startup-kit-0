d6854ab8db2306f148f9400d6e90dc21154868ef
Volo Builds <148304490+VoloBuilds@users.noreply.github.com>
2025-06-07 01:17:36 -0400

    Increment version

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ const program = new Command();
 
 // Default template URL
 const DEFAULT_TEMPLATE = 'https://github.com/VoloBuilds/volo-app.git';
-const VOLO_APP_BRANCH = 'release/v0.2.0';
+const VOLO_APP_BRANCH = 'release/v0.2.1';
 
 export async function main() {
   program
