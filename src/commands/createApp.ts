089ef7178c31dc33f3becd8c3052fe03f5e7d06a
Volo Builds <148304490+VoloBuilds@users.noreply.github.com>
2025-06-13 17:33:35 -0400

    Try pnpm with scripts

--- src/commands/createApp.ts
+++ src/commands/createApp.ts
@@ -81,7 +81,7 @@ export async function createApp(
   }).start();
   
   try {
-    await execPnpm(['install'], { 
+    await execPnpm(['install', '--enable-scripts'], { 
       cwd: directory, 
       stdio: options.verbose ? 'inherit' : 'pipe' 
     });
